93fb024a2a88746da6e67e8a1b14a209428ffb17
Beef Webster <c_goldsmith@yahoo.com>
2022-10-24 18:31:07 -0700

    added inquirer questions

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,32 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = ["test"];
+const questions = inquirer.prompt([
+    {
+        type: "input",
+        name: "motivation",
+        message: "What motivated you to make this?"
+    },
+    {
+        type: "input",
+        name: "solve",
+        message: "What problem does this solve?" 
+    },
+    {
+        type: "input",
+        name: "learn",
+        message: "What did you learn?"
+    },
+    {
+        type: "input",
+        name: "motivation",
+        message: "What problem does this solve?" 
+    }
+]).then((data) => {
+    fs.writeFile("README.md", JSON.stringify(data), (err) => {
+        err ? console.log(err) : console.log("Saved succesfully.")
+    })
+});
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
