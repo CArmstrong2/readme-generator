ea694cde7220568a55492c29eeb3e73fd9b0a44e
Beef Webster <c_goldsmith@yahoo.com>
2022-10-25 19:52:28 -0700

    added inquirer questions and file-write function

--- Develop/index.js
+++ Develop/index.js
@@ -2,39 +2,48 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = inquirer.prompt([
-    {
-        type: "input",
-        name: "motivation",
-        message: "What motivated you to make this?"
-    },
-    {
-        type: "input",
-        name: "solve",
-        message: "What problem does this solve?" 
-    },
-    {
-        type: "input",
-        name: "learn",
-        message: "What did you learn?"
-    },
-    {
-        type: "input",
-        name: "motivation",
-        message: "What problem does this solve?" 
-    }
-]).then((data) => {
-    fs.writeFile("README.md", JSON.stringify(data), (err) => {
-        err ? console.log(err) : console.log("Saved succesfully.")
-    })
-});
-
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    const questions = inquirer.prompt([
+        {
+            type: "input",
+            name: "q0",
+            message: "What is the project name?"
+        },
+        {
+            type: "input",
+            name: "q1",
+            message: "What motivated you to make this?"
+        },
+        {
+            type: "input",
+            name: "q2",
+            message: "Why did you build this?" 
+        },
+        {
+            type: "input",
+            name: "q3",
+            message: "What did you learn?"
+        },
+        {
+            type: "input",
+            name: "q4",
+            message: "What problem does this solve?" 
+        }
+    ]).then((data) => {
+        const output = `# ${data['q0']}\n\n## Project Description\n\n${data['q1']}${data['q2']}\n\n${data['q3']}\n\n${data['q4']}\n\n## Credits: Christopher Armstrong`
+
+        fs.writeFile("README.md", (output), (err) => 
+        {
+        err ? console.log(err) : console.log("Saved succesfully.")
+        })
+    });
+}
 
 // Function call to initialize app
 init();
